99c0134788b23a35e5011d8f0b107f0462cae92a
Jacob Rosenthal <jakerosenthal@gmail.com>
2013-07-30 00:35:43 -0700

    created a menu in test to trigger actions from console

--- test_pyhaptic.py
+++ test_pyhaptic.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import sys
 
 from pyhaptic import HapticInterface
 
@@ -11,6 +12,50 @@ def find_comm_port():
         	print i
     	comm_choice = raw_input("\nPlease choose the full path to the comm port that the haptic controller is connected to:") 
     return comm_choice
+def function_zero():
+    print "running zero"
+    two_d_display.vibrate(0,0,0,1)
+    two_d_display.vibrate(1,0,0,1)
+    two_d_display.vibrate(2,0,0,1)
+    two_d_display.vibrate(3,0,0,1)
+
+    print "completed zero"
+
+def function_one():
+    print "running one"
+    print "completed one"
+
+def function_two():
+    print "running two"
+    print "completed two"
+
+def function_three():
+    print "running three"
+    print "completed three"
+
+def function_four():
+    print "running four"
+    print "completed four"
+
+def function_five():
+    print "running five"
+    print "completed five"
+
+def function_six():
+    print "running six"
+    print "completed six"
+
+def function_seven():
+    print "running seven"
+    print "completed seven"
+
+def function_eight():
+    print "running eight"
+    print "completed eight"
+
+def function_nine():
+    print "running nine"
+    print "completed nine"
 
 if __name__ == '__main__':
     
@@ -19,8 +64,32 @@ if __name__ == '__main__':
     	two_d_display.connect()
     except:
         print "Failed to connect on ..."
-    print "Vibrating Motors"
-    two_d_display.vibrate(0,0,0,1)
-    two_d_display.vibrate(1,0,0,1)
-    two_d_display.vibrate(2,0,0,1)
-    two_d_display.vibrate(3,0,0,1)
+        sys.exit(1)
+    
+    print "enter a number 0-9 to activate that function"
+    print "anything else to exit"
+    while True:
+        comm_choice = sys.stdin.read(1)
+        sys.stdin.read(1) #dump the newline char
+        if comm_choice == "0":
+            function_zero()
+        elif comm_choice == "1":
+            function_one()
+        elif comm_choice == "2":
+            function_two()
+        elif comm_choice == "3":
+            function_three()
+        elif comm_choice == "4":
+            function_four()
+        elif comm_choice == "5":
+            function_five()
+        elif comm_choice == "6":
+            function_six()
+        elif comm_choice == "7":
+            function_seven()
+        elif comm_choice == "8":
+            function_eight()
+        elif comm_choice == "9":
+            function_nine()
+        else:
+            sys.exit(0)
